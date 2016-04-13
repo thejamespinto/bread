a7e2875850ab598cdc6f767ed399a1e8a1f54a55
James Pinto <thejamespinto@gmail.com>
2016-04-13 19:19:29 -0300

    big refactor WIP

--- lib/bread.rb
+++ lib/bread.rb
@@ -7,7 +7,6 @@ require "bread/configuration"
 require "bread/controller"
 require "bread/data/crumb"
 require "bread/manager/manager"
-require "bread/manager/actions"
 require "bread/manager/actions/controller_scope"
 require "bread/manager/actions/action_scope"
 require "bread/manager/crumbs"
@@ -19,12 +18,14 @@ require 'colorize'
 
 
 module Bread
-  def self.actions
-    Manager.instance.actions
-  end
-
   def self.crumbs
     Manager.instance.crumbs
+  end
+
+  def self.for_controller(controller)
+    reload!
+    crumbset = configuration.get_controller_scope(controller.controller_path).get_crumbset(controller.action_name)
+    crumbs.crumbs_for(controller, crumbset)
   end
 
   def self.reload!
